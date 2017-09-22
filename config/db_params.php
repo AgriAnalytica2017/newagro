a2fe247496eaaf5dc54637f349b69b7e42b7e9a5
stas383 <stasik1998i@gmail.com>
2017-09-22 16:03:02 +0300

    Update db_params.php

--- config/db_params.php
+++ config/db_params.php
@@ -1,15 +1,5 @@
 <?php
 return array(
-//          'host' => 'sscukr00.mysql.ukraine.com.ua',
-//          'dbname' => 'sscukr00_newagro',
-//          'user' => 'sscukr00_newagro',
-//          'password' => '',
-
-//			'host' => 'localhost',
-//			'dbname' => 'agro_new',
-//			'user' => 'root',
-//			'password' => '',
-
           'host' => 's25.thehost.com.ua',
           'dbname' => 'Agro',
           'user' => 'test_agro',
