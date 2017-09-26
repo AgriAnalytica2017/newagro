1873d7d810b773563675cf5691998d24ddac8545
Nazar <stasik1998i@gmail.com>
2017-09-26 14:31:53 +0300

    Version 26.09.17

--- cabinet/new-farmer/controllers/PlanController.php
+++ cabinet/new-farmer/controllers/PlanController.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 /**
  * Created by PhpStorm.
  * User: Иван
@@ -7,11 +7,8 @@
  */
 class PlanController{
     public function actionIndex(){
-<<<<<<< HEAD
         SRC::template('new-farmer','newTemplate','newFieldManagement');
-=======
         SRC::template('new-farmer','new','plan');
->>>>>>> 162aeb70703e489698c6eafa180fd33a932a0abb
         return true;
     }
 }
